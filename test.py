4a84a84bf17745d4fb7f00b5f88f78d7cb1ffeda
Teemu Kurki <teemukurki4@gmail.com>
2017-09-02 18:34:04 +0300

    added Stanford CoreNLP sentiment analysis

--- test.py
+++ test.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python
 import textract
 import sys
+from pycorenlp import StanfordCoreNLP
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def pdfToText(pathToPdf):
+    nlp = StanfordCoreNLP("http://localhost:9000")
     text = textract.process(pathToPdf)
 
-    start = text.find("Toimitusjohtaja")
+    start = text.find("In the second quarter of the year")
     end = text.find("\n\n",start)
 
     with open("wholeDocument.txt", "w") as file:
         file.write(text)
     with open("tfMietteet.txt", "w") as file:
         file.write(text[start:end])
+
+    foundText = text[start:end]
+
+    nlpOutput = nlp.annotate(text,properties={
+        "annotators": "sentiment",
+        "outputFormat": "json",
+    })
+    for s in nlpOutput["sentences"]:
+        print "%d: '%s': %s %s" % (
+            s["index"],
+            " ".join([t["word"] for t in s["tokens"]]),
+            s["sentimentValue"], s["sentiment"])
+
+
 
 if len(sys.argv) < 2:
     print("Add path to pdf file")
